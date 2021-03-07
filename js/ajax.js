ad9d9e04d084ef8200caf9e22cdd1bcc3f22374c
luisangeluis <luisangeluis@gmail.com>
2021-03-06 20:47:17 -0600

    Se revisa status y y los numeros de estado revisar los ultimos

---
+++ js/ajax.js
@@ -0,0 +1,24 @@
+(()=>{
+    const xhr = new XMLHttpRequest();
+    $xhr = document.getElementById('xhr');
+    $fragment = document.createDocumentFragment();
+
+    xhr.addEventListener('readystatechange',(e)=>{
+
+        if(xhr.readyState!==4)return;
+        //console.log(xhr);
+
+        if(xhr.status>=200 && xhr.status<400){
+            //console.log(xhr);
+            console.log('exito');
+            console.log(xhr.responseText);
+
+        }else{
+            console.log('error');
+
+        }
+
+    });
+    xhr.open('GET','https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+})();
