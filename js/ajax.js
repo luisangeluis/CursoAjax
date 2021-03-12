4d98f43ed497ce3dcdf974175ae182b5bc72fbb5
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-03-12 17:28:35 -0600

    Se revisa hasta la parte de JSON.parse

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,4 @@
+/*
 (()=>{
     const xhr = new XMLHttpRequest();
     $xhr = document.getElementById('xhr');
@@ -9,7 +10,7 @@
         //console.log(xhr);
 
         if(xhr.status>=200 && xhr.status<400){
-            //console.log(xhr);
+            console.log(xhr);
             console.log('exito');
             console.log(xhr.responseText);
 
@@ -21,4 +22,37 @@
     });
     xhr.open('GET','https://jsonplaceholder.typicode.com/users');
     xhr.send();
+})();
+*/
+(()=>{
+    //alert('hola');
+    const xhr = new XMLHttpRequest();
+    $xhr = document.getElementById('xhr');
+    $fragment = document.createDocumentFragment();
+
+
+    xhr.addEventListener('readystatechange',(e)=>{
+        if(xhr.readyState!==4)
+        return;
+        console.log(xhr);
+
+        if(xhr.status>=200 && xhr.status<300){
+            //console.log('exito');
+            //console.log(xhr.responseText);
+            let json =JSON.parse(xhr.responseText);
+            console.log(json);
+        }else{
+            console.log('error');
+        }
+        
+       
+
+    });
+
+    xhr.open('GET','https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+
+
+
+
 })();
