74c877edc5279dbded9c2ce444d5fbdfd7944dbb
luisangeluis <luisangeluis@gmail.com>
2021-03-15 21:53:11 -0600

    Se esta revisando video 107 peticiones fetch pendiente

--- js/ajax.js
+++ js/ajax.js
@@ -37,7 +37,7 @@
         //console.log(xhr);
 
         if(xhr.status>=200 && xhr.status<300){
-            console.log('exito');
+            //console.log('exito');
             //console.log(xhr.responseText);
             
             let json =JSON.parse(xhr.responseText);
@@ -59,12 +59,36 @@
 
     });
 
-   // xhr.open('GET','https://jsonplaceholder.typicode.com/users');
-    xhr.open('GET','/CURSOAJAX/js/api.json');
+   xhr.open('GET','https://jsonplaceholder.typicode.com/users');
+    //xhr.open('GET','/CURSOAJAX/js/api.json');
 
     xhr.send();
 
 
 
 
+})();
+
+(()=>{
+
+    //METODO FETCH
+    const $fetch = document.querySelector('#fetch');
+    $fragment = document.createDocumentFragment();
+    //THEN RESPUESTA EXITOSA
+    fetch('https://jsonplaceholder.typicode.com/user').then((res)=>{
+        //TE MANDA LA INFO EN FORMATO READABLESTREAM
+        console.log(res);
+        
+        //VALIDACION PARA QUE SE EJECUTE EL CATCH EN CASO QUE APLIQUE
+        return res.ok?res.json():Promise.reject(res);
+        
+    }).then((json)=>{
+        console.log(json);
+    }).catch((err)=>{
+        console.log("Estamos en el catch: ",err);
+        let message = err.statusText || "Ocurrio un error";
+        $fetch.innerText =`Error: ${err.status}: ${message}`;
+    }).finally(()=>{
+        console.log('Esto se ejecuta independientemente del resultado de la promesa Fetch');
+    });
 })();
